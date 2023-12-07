36da5b54a5576047a06bec9083443bcccd5ab046
ZalivkoDenis <ZalivkoDenis@gmail.com>
2023-12-07 19:32:40 +0300

    Update views in all apps

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.template import loader
+
 from .models import Task
 
 
 def tasks_list(request: HttpRequest) -> HttpResponse | None:
-    template = loader.get_template("tasks/list.html")
-    return HttpResponse(template.render({"tasks": Task.objects.all()}))
+    # template = loader.get_template("tasks/list.html")
+    # return HttpResponse(template.render({"tasks": Task.objects.all()}))
+    return render(request, "tasks/list.html", {"tasks": Task.objects.all()})
 
 
 def task_detail(request: HttpRequest, task_id: int) -> HttpResponse | None:
-    return HttpResponse(loader.get_template("tasks/detail.html").render({"task": Task.objects.get(id=task_id)}))
+    # return HttpResponse(loader.get_template("tasks/detail.html").render({"task": Task.objects.get(id=task_id)}))
+    return render(request, "tasks/detail.html", {"task": Task.objects.get(id=task_id)})
