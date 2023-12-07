f250e0fecb5b1497ef2b9eba7b1c669cfa2fa63c
ZalivkoDenis <ZalivkoDenis@gmail.com>
2023-12-07 19:32:40 +0300

    Update views in all apps

--- index/views.py
+++ index/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.template import loader
 
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse | None:
-    return HttpResponse(loader.get_template("index.html").render())
+    # return HttpResponse(loader.get_template("index.html").render())
+    return render(request, "index.html")
--- users/views.py
+++ users/views.py
@@ -6,12 +6,15 @@ from .models import User
 
 
 def user_list(request: HttpRequest) -> HttpResponse | None:
-    template = loader.get_template("users/list.html")
+    # template = loader.get_template("users/list.html")
     try:
-        return HttpResponse(template.render({"users": User.objects.all()}))
+        # return HttpResponse(template.render({"users": User.objects.all()}))
+        return render(request, "users/list.html", {"users": User.objects.all()})
     except Exception as e:
-        return HttpResponse(template.render({"error_message": f"Error: {e}"}))
+        # return HttpResponse(template.render({"error_message": f"Error: {e}"}))
+        return render(request, "users/list.html", {"error_message": f"Error: {e}"})
 
 
 def user_detail(request: HttpRequest, user_id: int) -> HttpResponse | None:
-    return HttpResponse(loader.get_template("users/detail.html").render({"user": User.objects.get(id=user_id)}))
+    # return HttpResponse(loader.get_template("users/detail.html").render({"user": User.objects.get(id=user_id)}))
+    return render(request, "users/detail.html", {"user": User.objects.get(id=user_id)})
